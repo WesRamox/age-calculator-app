b835a82495ef68406e300d77e7648dbdad6fbf7a
WesRamox <wesleyramox258@gmail.com>
2024-04-30 19:06:23 -0300

    Update

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,23 +1,45 @@
-let yearSelected = document.getElementById("year")
-let monthSelected = document.getElementById("month")
-let daySelected = document.getElementById("day")
+let resultYears = document.getElementById("resultYears")
+let resultMonths = document.getElementById("resultMonths")
+let resultDays = document.getElementById("resultDays")
 
 let btnSubmit = document.getElementById("submit");
 
-let today = new Date()
+function ageCalculator() {
+    let yearSelected = document.getElementById("year").value
+    let monthSelected = document.getElementById("month").value
+    let daySelected = document.getElementById("day").value
 
+    if(yearSelected != "" && monthSelected != "" && daySelected != "") {
+        var date = new Date();
+        var currentDay = date.getDate();
+        var currentMonth = 1 + date.getMonth();
+        var currentYear = date.getFullYear();
+        var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+        if (daySelected > currentDay) {
+            currentDay = currentDay + month[currentMonth - 1];
+            currentMonth = currentMonth - 1;
+        }
+
+        if (monthSelected > currentMonth) {
+            currentMonth = currentMonth + 12;
+            currentYear = currentYear - 1;
+        }
+
+        var dResult = currentDay - daySelected;
+        var mResult = currentMonth - monthSelected;
+        var yResult = currentYear - yearSelected;
+
+        resultYears.innerHTML = yResult;
+        resultMonths.innerHTML = mResult;
+        resultDays.innerHTML = dResult;
+    } else {
+        console.error()
+    }
+}
 
 btnSubmit.addEventListener("click", () => {
-    let yearUpdated = Number(yearSelected.value)
-    let monthUpdated = Number(monthSelected.value)
-    let dayUpdated = Number(daySelected.value)
-    
-    if(dayUpdated <= 31 && monthUpdated <= 12 && yearUpdated <= 2024) {
-        let dateTotal = dayUpdated + "/" + monthUpdated + "/" + yearUpdated
-        console.log(dateTotal)
-    } else {
-        console.log("error")
-    }
-    calculaIdade(yearUpdated, monthUpdated, dayUpdated)
+    ageCalculator();
 })
+
+ageCalculator();
